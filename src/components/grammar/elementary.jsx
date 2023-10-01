70aaa99b2ddf1fc641cbb96e9fc1dd697cd26bce
1sfandiyor <isfandiyor06112000@gmail.com>
2023-10-01 10:46:42 +0500

    Translated descriptions of lesson cards

--- src/components/grammar/elementary.jsx
+++ src/components/grammar/elementary.jsx
@@ -43,24 +43,24 @@ export default function Elementary() {
 </h1>
         <div className='container'>
             <div className='card-wrapper flex justify-around items-center flex-wrap gap-y-5 pb-[20px]'>
-                <LessonCards img={e1} text={"Present simple forms of 'to be' – am/is/are"} path={'grammar/a1/to-be'}/>
+                <LessonCards img={e1} text={"'To be' ning Present Simple shakli – am/is/are"} path={'grammar/a1/to-be'}/>
                 <LessonCards img={e2} text={"This, that, these, those"} path={'grammar/a1/this-that-these-those'}/>
-                <LessonCards img={e3} text={'Possessive adjectives and subject pronouns (I/my, you/your, etc.)'} path={'grammar/a1/possive-adjectives'}/>
-                <LessonCards img={e4} text={'A/an, plurals – singular and plural forms'} path={'grammar/a1/a-an-plurals'} />
-                <LessonCards img={e5} text={'Adjectives – old, interesting, expensive, etc.'} path={'grammar/a1/adjectives'} />
+                <LessonCards img={e3} text={'Possessive adjective-lar va subject pronoun-lar (I/my, you/your, etc.)'} path={'grammar/a1/possive-adjectives'}/>
+                <LessonCards img={e4} text={'A/an, ko\'plik – birlik va ko\'plik shakli'} path={'grammar/a1/a-an-plurals'} />
+                <LessonCards img={e5} text={'Adjective-lar – old, interesting, expensive, etc.'} path={'grammar/a1/adjectives'} />
                 <LessonCards img={e6} text={"Present simple – I do, I don't, Do I?"} path={'grammar/a1/present-simple'} />
                 <LessonCards img={e7} text={'Savol tuzish – gap ketma ketligi va so\'roq gaplar'} path={'grammar/a1/questions'} />
-                <LessonCards img={e8} text={'Present simple-da ishlatiladigan qo\'shimchalalr'} path={'grammar/a1/adverbs-frequency'} />
-                <LessonCards img={e9} text={'Egalik olmoshlari vs kishilik olmoshlari – me yoki I, she yoki her?'} path={'grammar/a1/object-pronouns/'} />
-                <LessonCards img={e10} text={"Whose, possessive 's – Whose is this? It's Mike's"} path={'grammar/a1/whose-possessive-s'} />
+                <LessonCards img={e8} text={'Present simple-da ishlatiladigan qo\'shimchalar'} path={'grammar/a1/adverbs-frequency'} />
+                <LessonCards img={e9} text={'Object pronoun-lar vs subject pronoun-lar – me yoki I, she yoki her?'} path={'grammar/a1/object-pronouns/'} />
+                <LessonCards img={e10} text={"Whose va 's qo'shimchasi  – Whose is this? It's Mike's"} path={'grammar/a1/whose-possessive-s'} />
                 <LessonCards img={e11} text={'At, in, on – vaqt predloglari'} path={'grammar/a1/at-in-on-prepositions-time'} />
                 <LessonCards img={e12} text={'At, in, on – joylashuv predloglari'} path={'grammar/a1/at-in-on-prepositions-of-place'} />
                 <LessonCards img={e13} text={"Can, can't – ability, possibility, permission"} path={'grammar/a1/can-cant'} />
                 <LessonCards img={e14} text={"Present continuous – I'm doing, I'm not doing, Are you doing?"} path={'grammar/a1/present-continuous'} />
-                <LessonCards img={e15} text={'Present simple yoki present continuous?'} path={'grammar/a1/present-simple-present-continuous'} />
+                <LessonCards img={e15} text={'Present simple yoki present continuous ?'} path={'grammar/a1/present-simple-present-continuous'} />
                 <LessonCards img={e16} text={'Buyruq gaplar – Sit down! Don’t talk!'} path={'grammar/a1/imperative-sit-dont-talk'} />
-                <LessonCards img={e17} text={"Was/were – 'be' ning past simple shakli"} path={'grammar/a1/was-were'} />
-                <LessonCards img={e18} text={'Past simple – to\'g\'ri/noto\'g\'ri fe\'llar'} path={'grammar/a1/past-simple-regular-irregular'} />
+                <LessonCards img={e17} text={"Was/were – 'to be' ning past simple shakli"} path={'grammar/a1/was-were'} />
+                <LessonCards img={e18} text={'Past simple – to\'g\'ri va noto\'g\'ri fe\'llar'} path={'grammar/a1/past-simple-regular-irregular'} />
                 <LessonCards img={e19} text={'Past simple – inkor va so\'roq shakli'} path={'grammar/a1/past-simple-negatives-questions'} />
                 <LessonCards img={e20} text={'Fe\'l + to + infinitive va fe\'l + -ing'} path={'grammar/a1/verbs-infinitive-verbs-ing'} />
                 <LessonCards img={e21} text={"Would you like...? I'd like..."} path={'grammar/a1/would-like'} />
@@ -68,17 +68,17 @@ export default function Elementary() {
                 <LessonCards img={e23} text={'A, some, any – sanaladigan va sanalmaydigan otlar'} path={'grammar/a1/a-some-any-countable-uncountable'} />
                 <LessonCards img={e24} text={'There is, there are – there was, there were'} path={'grammar/a1/there-is-there-are'} />
                 <LessonCards img={e25} text={'There yoki it'} path={'grammar/a1/there-it'} />
-                <LessonCards img={e26} text={'Next to, under, between, in front of, behind, over, etc.'} path={'grammar/a1/next-to-under-between-in-front-behind-etc'} />
+                <LessonCards img={e26} text={'Next to, under, between, in front of, behind, over, va hokazo.'} path={'grammar/a1/next-to-under-between-in-front-behind-etc'} />
                 <LessonCards img={e27} text={'Much, many, a lot of, a little, a few'} path={'grammar/a1/much-many-lot-little-few'} />
-                <LessonCards img={e28} text={'Qiyosiy sifatlar – older than, more important than, etc.'} path={'grammar/a1/comparative-adjectives'} />
+                <LessonCards img={e28} text={'Qiyosiy sifatlar – older than, more important than, va hokazo.'} path={'grammar/a1/comparative-adjectives'} />
                 <LessonCards img={e29} text={'Orttirma sifatlar – the oldest, the most important, etc.'} path={'grammar/a1/superlative-adjectives'} />
                 <LessonCards img={e30} text={"'Will' va 'shall' – Future"} path={'grammar/a1/will-and-shall'} />
                 <LessonCards img={e31} text={'Be going to – rejalar va istaklar'} path={'grammar/a1/be-going-to-plans-predictions'} />
                 <LessonCards img={e32} text={'(slowly) – or adjectives (slow)?'} path={'grammar/a1/adverbs-manner'} />
-                <LessonCards img={e33} text={'A/an, the, no article – the use of articles in English'} path={'grammar/a1/a-an-the-no-article'} />
-                <LessonCards img={e34} text={'Conjunctions: and, but, or, so, because'} path={'grammar/a1/conjunctions_and-but-or-so-because'} />
+                <LessonCards img={e33} text={'A/an, the, no article – Ingliz tilida artikllardan foydalanish'} path={'grammar/a1/a-an-the-no-article'} />
+                <LessonCards img={e34} text={'Bo\'g\'lovchilar: and, but, or, so, because'} path={'grammar/a1/conjunctions_and-but-or-so-because'} />
                 <LessonCards img={e35} text={'Ingliz tilida so\'zlarning asosiy tartibi'} path={'grammar/a1/basic-word-order-in-english'} />
-                <LessonCards img={e36} text={'Gramatik kontent jdavali – A1'} path={'grammar/a1/'} />
+                <LessonCards img={e36} text={'Gramatika mavzularining jadavali – A1'} path={'grammar/a1/'} />
             </div>
         </div>
     </div>
--- src/components/lessons.jsx
+++ src/components/lessons.jsx
@@ -12,19 +12,19 @@ export default function Lessons() {
     <div className='container2 mt-10'>
         <h2 className='text-center mb-10 text-[35px] font-bold'>Bugun nima <span className='text-[40px] text-[#50af31]'>o&apos;rganmoqchisiz?</span></h2>
         <div className='flex justify-between items-center flex-wrap'>
-            <TopicCards img={grammar} topic={'Grammar Lesson'} desc={'Study the grammar lessons that are typically included in each level: A1, A2, B1, B1+, B2. There are three or more exercises and an explanation in each lesson, and feedback for every single question!'} color={'1a8ec1'} path={'grammar'} />
+            <TopicCards img={grammar} topic={'Gramatika darslari'} desc={'Har bir darajani o\'z ichiga olgan oddiy grammatika darslarini o\'rganing: A1, A2, B1, B1+, B2. Har bir darsda uchta yoki undan ortiq mashqlar, tushuntirib berishlar va har bir savol uchun fikr-mulohazalar mavjud!'} color={'1a8ec1'} path={'grammar'} />
 
-            <TopicCards img={listening} topic={'Listening Lesson'} desc={'Improve your listening skills by practicing with audio and video tests. There are tests for each level: A1, A2, B1, B1+, B2. You will be able to see the transcription of the audio after you submit your answers.'} color={'fecc00'} path={'listening'} />
+            <TopicCards img={listening} topic={'Listening darslari'} desc={"Audio va video testlar bilan listening bo'yicha ko'nikmalaringizni kuchaytiring. Har bir darajaga mos testlar mavjud: A1, A2, B1, B1+, B2. Javoblaringizni yuborganingizdan so'ng audioning transkripsiyasini ko'rishingiz mumkin bo'ladi."} color={'fecc00'} path={'listening'} />
 
-            <TopicCards img={reading} topic={'Reading Lesson'} desc={'Need to improve your reading skills? Work on our reading tests. There are reading tests for A1, A2, B1, B1+ and B2. You will find different types of texts and there are different types of questions in each test'} color={'ed1c24'} path={'reading'} />
+            <TopicCards img={reading} topic={'Reading darslari'} desc={"Reading bo'yicha ko'nikmalaringizni oshirmoqchimisiz ? Bu yerda A1, A2, B1 va B2 darajalar uchun reading teslar mavjud, Har bir testda har xil savol turlari va matnlarni topishingiz mumkin."} color={'ed1c24'} path={'reading'} />
 
-            <TopicCards img={useofenglish} topic={'Use Of English'} desc={'In each of these tests you will find 15 multiple-choice questions about the different grammar lessons that you have studied for a specific level: A1, A2, B1, B1+ and B2.  You will get feedback for every answer.'} color={'ec6825'} path={'useofenglish'} />
+            <TopicCards img={useofenglish} topic={'Ingliz tilidan foydalanish'} desc={"Ushbu testlarning har birida ma'lum bir daraja uchun o'rgangan turli grammatika darslaringiz bo'yicha 15 ta ko'p tanlovli savollarni topasiz: A1, A2, B1, B1+ va B2. Har bir javob uchun fikr-mulohaza olasiz."} color={'ec6825'} path={'useofenglish'} />
 
-            <TopicCards img={writing} topic={'Writing Lesson'} desc={'Improve your writing with the exercises suggested in each lesson. Different types of texts for each level: A1, A2, B1, B1+, or B2. You will learn how to organise and connect the text in your compositions.'} color={'a84d98'} path={'writing'} />
+            <TopicCards img={writing} topic={'Writing darslari'} desc={"Har bir darsda tavsiya etilgan mashqlar yordamida writing bilimlaringizni yaxshilang. Har bir daraja uchun har xil turdagi matnlar mavjud : A1, A2, B1, B1+ yoki B2. Siz o'z ... matnni qanday tuzish va bir biriga bog'lashni o'rganasiz."} color={'a84d98'} path={'writing'} />
 
-            <TopicCards img={exams} topic={'Exams'} desc={'Improve your exam skills and achieve better results with our practice materials. Test-English will help you with your Cambridge KEY, Cambridge Preliminary, Cambridge First, IELTS, or TOEFL iBT exam.'} color={'50af31'} path={'exams'} />
+            <TopicCards img={exams} topic={'Imtihonlar'} desc={"Bizning amaliy materiallarimiz yordamida imtihon topshirish ko'nikmalaringini oshiring va yaxshi natijalarga erishing. Test-English sizga Cambridge KEY, Cambridge Preliminary, Cambridge First, IELTS yoki TOEFL iBT imtihonlarida yordam beradi."} color={'50af31'} path={'exams'} />
 
-            <TopicCards img={leveltest} topic={'Level Test'} desc={"If you are not sure about your level, don't worry, you can take this online test. There are 60 multiple-choice questions and there is no time limit. You will be able to see the answers when you finish the test."} color={'50af31'} path={'leveltest'} />
+            <TopicCards img={leveltest} topic={'Darajani aniqlash testi'} desc={"Darajangi qandayligini bilmasangiz havaotir olamang, siz ushbu online testni topshirib ko'rishingiz mumkin. Bu yerda 60-ta ko'p tanlovli savollar mavjuda va hech qanday vaqt belgilanmangan. Testni tugatganingizdan so'ng javoblarni ko'rishingiz mumkin."} color={'50af31'} path={'leveltest'} />
 
         </div>
     </div>
